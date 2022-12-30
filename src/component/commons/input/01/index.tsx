81db73467e580a3aed2ea0a38849f45cf746386c
mnin97 <rudals010447@gmail.com>
2022-12-30 14:08:11 +0900

    [ feat ] 인풋공통컴포넌트추가

---
+++ src/component/commons/input/01/index.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const Input = styled.input`
+  width: 83%;
+  height: 70px;
+  border-radius: 4px;
+  border: 1px solid rgb(48 100 10);
+  padding: 18px;
+`;
+
+export default function input01() {
+  return <Input />;
+}
