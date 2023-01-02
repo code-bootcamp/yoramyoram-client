430902e86fa5c46273fbd45211313a29644bccbd
mnin97 <rudals010447@gmail.com>
2023-01-02 10:54:50 +0900

    [ feat ] 공통컴포넌트 추가

---
+++ src/component/commons/Join/Input01.styles.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  color: rgb(48 100 10);
+  margin-top: 37px;
+`;
+
+export const NameInput = styled.input`
+  width: 80%;
+  height: 70px;
+  border-radius: 4px;
+  border: 1px solid rgb(48 100 10);
+  padding: 18px;
+`;
+
+export const Label = styled.label`
+  width: 20%;
+`;
---
+++ src/component/commons/inputs/accoutsearch/input02.styles.ts
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+
+export const SearchWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+  margin-bottom: 18px;
+  color: #222222;
+`;
+
+export const Label = styled.label`
+  width: 20%;
+  text-align: initial;
+`;
+
+export const SearchInput = styled.input`
+  width: 80%;
+  height: 63px;
+  border: 1px solid #b7b7b7;
+  border-radius: 4px;
+  background-color: none;
+  background: transparent;
+`;
