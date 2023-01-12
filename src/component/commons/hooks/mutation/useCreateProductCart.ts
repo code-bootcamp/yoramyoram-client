fb87b3d0292bbfdd1baf7bea409b8687ae1eab50
redcatsh <redminnie56@naver.com>
2023-01-12 16:10:27 +0900

    [ feat ] 장바구니 옵션넣기

--- src/component/commons/hooks/mutation/useCreateProductCart.ts
+++ src/component/commons/hooks/mutation/useCreateProductCart.ts
@@ -1,10 +1,26 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_PRODUCT_CART = gql`
-  mutation createProductCart($productId: String!) {
-    createProductCart(productId: $productId) {
+  mutation createProductCart(
+    $productId: String!
+    $etc1Name: String
+    $etc1Value: String
+    $etc2Name: String
+    $etc2Value: String
+  ) {
+    createProductCart(
+      productId: $productId
+      etc1Name: $etc1Name
+      etc1Value: $etc1Value
+      etc2Name: $etc2Name
+      etc2Value: $etc2Value
+    ) {
       id
       quantity
+      etc1Name
+      etc1Value
+      etc2Name
+      etc2Value
     }
   }
 `;
