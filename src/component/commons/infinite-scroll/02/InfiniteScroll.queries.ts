a4e58fd24409e5d2920a75023795cc83620c28a2
mnin97 <rudals010447@gmail.com>
2023-01-15 16:10:13 +0900

    [ test ] test

---
+++ src/component/commons/infinite-scroll/02/InfiniteScroll.queries.ts
@@ -0,0 +1,18 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export const FETCH_BOARDS_COUNT = gql`
+  query {
+    fetchBoardsCount
+  }
+`;
